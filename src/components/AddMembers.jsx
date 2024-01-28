31c751b92c0fc9f14f4c7597d85abbbdcbfb88ea
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-01-28 23:20:01 +0900

    group component UI test setup & implementation

---
+++ src/components/AddMembers.jsx
@@ -0,0 +1,12 @@
+import {CenteredOverlayForm} from "./CenteredOverlayForm";
+
+export const AddMembers = () => {
+    return (
+        <div>
+            AddMembers component
+
+            <CenteredOverlayForm />
+
+        </div>
+    )
+}
